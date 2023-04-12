faf181df7ea17983d3f619248f5cbd9d1fec8923
Denis <denis_im@mail.ru>
2023-04-12 20:51:27 +0500

    определен минимум и максимум двух чисел

---
+++ Task 1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число a:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите число b:");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Максимальное из двух чисел:" +Math.Max(a,b));
+
+
+Console.Write("Минимальное из двух чисел:" +Math.Min(a,b));
